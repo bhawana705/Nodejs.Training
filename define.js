ee18ecd54a2737742075cf171d652682c6bb38b2
Bhawana Pandey <bhawana321pandey@gmail.com>
2025-12-09 14:02:42 +0545

    express js

---
+++ define.js
@@ -0,0 +1,19 @@
+// TRY = sesnitive code 
+
+// Throw =n error in catch 
+
+// async baneko jaba samma await run hunna taba samma  kunai pani value run hunna 
+
+//STD method  = get and post 
+// get =  server bata lini
+// post =  form summision 
+ 
+// middleware = login , authorized
+
+//  array = collection of element 
+
+// error handaling = add.event.listener
+
+
+// DOM =  html , head, titlr , body , div , p, h1 ,ul, 
+// 
--- server.js
+++ server.js
@@ -247,10 +247,20 @@
 
 
 
-//  moss
-// brocode
-// 
+// EXAMPLE
 
+let a=10;
+let b=20;
+let c=30;
 
+ function test(){
+    let a=10;
+    const b=20;
+    var c =30;
 
+ }
 
+ test();
+ console.log(a);
+ console.log(b);
+ console.log(c);
